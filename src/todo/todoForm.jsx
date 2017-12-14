cf1a8e97f16249c0360167a7aca89efdd381d128
Unknown <oliveira.ambrozio@gmail.com>
2017-12-14 13:31:07 -0200

    post handleAdd OK

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -7,10 +7,13 @@ export default props => (
     <div role='form' className='todoForm'>
         <Grid cols='12 9 10'>
             <input id='description' className='form-control'
-                placeholder='Descrição'/>
+                placeholder='Descrição' 
+                onChange={ props.handleChange }
+                value={props.description}/>
         </Grid>
         <Grid cols='12 3 2'>
-            <Button style='primary' icon='plus'/>
+            <Button style='primary' icon='plus'
+                onClick={props.handleAdd}/>
         </Grid>
     </div>
 )
